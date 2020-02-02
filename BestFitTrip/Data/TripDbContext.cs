5141a439b327a4a20e64a800403cb0622ee072f9
Francesca F <fodfran@gmail.com>
2020-02-02 09:58:06 -0500

    Update TripDbContext.cs

--- BestFitTrip/Data/TripDbContext.cs
+++ BestFitTrip/Data/TripDbContext.cs
@@ -10,22 +10,14 @@ namespace BestFitTrip.Data
         public DbSet<User> Users { get; set; }
         public DbSet<DestinationValue> DestinationValues { get; set; }
 
-
         public TripDbContext(DbContextOptions<TripDbContext> options)
             : base(options)
         {
         }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<DestinationValue>()
-        //        .HasNoKey();
-        //}
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=localhost,1433; Database=BestFitDb; User=SA; Password=<YourStrong@Passw0rd>");
-
         }
     }
 }
